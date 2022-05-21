d360efe3382304ca76d66e3e73d79b487ff8092b
Marius <Marius@DESKTOP-RPDOLTQ>
2022-05-21 22:52:09 +0300

    chat added

--- src/app/admin/submissions/submission-detail/submission-detail.component.ts
+++ src/app/admin/submissions/submission-detail/submission-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-submission-detail',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubmissionDetailComponent implements OnInit {
 
+  @Input() submissionId: any;
+  
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+
+  ngOnChanges() {
+    // if(this.submissionId.replies) {
+    //   console.log(this.submissionId.replies)
+    //   this.submissionId.replies = [];
+    // }
+
   }
 
 }
